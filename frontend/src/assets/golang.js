7b6e48bd1cfbcd8137bc4b70590068c66d98e74d
Joseph Lewis III <joseph@josephlewis.net>
2025-03-10 22:03:12 -0700

    Adjust wasm loading to be relative to assets (#10)

--- frontend/src/assets/golang.js
+++ frontend/src/assets/golang.js
@@ -1,6 +1,6 @@
 import "./wasm_exec.js"
 
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch("./skilltreetool.wasm"), go.importObject).then((result) => {
+WebAssembly.instantiateStreaming(fetch("../skilltreetool.wasm"), go.importObject).then((result) => {
     go.run(result.instance);
 });
